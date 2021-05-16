7893310c7b5a6065a0c1846f1d580af08f914aba
Melvin Thomas <melvinchooranolil@gmail.com>
2021-05-16 19:14:20 +0530

    added beep booop and config.json

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const Discord = require('discord.js');
+// const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
@@ -8,10 +10,12 @@ client.once('ready', () => {
 client.on('message', message => {
 	console.log(message.content);
 
-    if (message.content === '!ping') {
+    if (message.content === `${prefix}ping`) {
 		// send back "Pong." to the channel the message was sent in
 		message.channel.send('Pong.');
-	}
+	} else if (message.content == `${prefix}beep`){
+        message.channel.send('Boop')
+    }
 
 });
 
@@ -22,4 +26,4 @@ client.on('message', message => {
 // 	}
 // });
 
-client.login('ODQzNDQwNTE3Nzk4NDI4Njgz.YKD5MQ.mpJ-EXZPDnWt5TcynWGwpWVSGKk');
+client.login(token);
